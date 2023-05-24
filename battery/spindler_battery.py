57e730ddcbb94ed36005edb29006e24ec5d6382e
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2023-05-24 04:11:51 -0500

    Upgraded spindler battery

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -8,7 +8,7 @@ class SpindlerBattery(BatteryInterface):
         self.last_service_date = last_service_date
         
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 3)
         if service_threshold_date < self.current_date:
             return True
         else:
