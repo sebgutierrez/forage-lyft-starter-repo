78839a0e8a319de68352f6b75480c8f57196ce0e
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2023-05-24 02:33:48 -0500

    Make methods static

--- car_factory.py
+++ car_factory.py
@@ -6,26 +6,31 @@ from battery.nubbin_battery import NubbinBattery
 from battery.spindler_battery import SpindlerBattery
 
 class CarFactory():
+    @staticmethod
     def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
         return Car(engine,battery)
-            
+    
+    @staticmethod        
     def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
         return Car(engine,battery)
-        
+    
+    @staticmethod    
     def create_palindrome(self, current_date, last_service_date, warning_light_on):
         engine = SternmanEngine(warning_light_on)
         battery = SpindlerBattery(current_date, last_service_date)
         return Car(engine,battery)
     
+    @staticmethod
     def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(current_date, last_service_date)
         return Car(engine,battery)
-        
+    
+    @staticmethod    
     def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(current_date, last_service_date)
