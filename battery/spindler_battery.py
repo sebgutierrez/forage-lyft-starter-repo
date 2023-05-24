449f688c9d8ec5f30a93c430e61428cb125a89bb
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2023-05-24 02:17:53 -0500

    Add __init__s

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -3,10 +3,13 @@ from battery import BatteryInterface
 from datetime import datetime
 
 class SpindlerBattery(BatteryInterface):
-
-    def needs_service(self, last_service_date, current_date):
+    def __init__(self, last_service_date, current_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+        
+    def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
-        if service_threshold_date < datetime.today().date():
+        if service_threshold_date < self.current_date:
             return True
         else:
             return False
